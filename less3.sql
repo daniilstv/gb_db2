7479cde9b9c7e1c318f5fc4e7b3b2f44ceee3de1
daniilstv <daniilstv@gmail.com>
2019-02-18 23:20:17 +0300

    less3.1: full data about city

---
+++ less3.sql
@@ -0,0 +1,16 @@
+USE geodata;
+
+SELECT
+    geodata._cities.title AS 'Город',
+    geodata._regions.title AS 'Регион',
+    geodata._countries.title AS 'Страна'
+
+    FROM geodata._cities 
+        JOIN (geodata._regions, geodata._countries)
+            ON (geodata._cities.region_id = geodata._regions.id 
+                AND geodata._cities.country_id = geodata._countries.id)
+    
+    WHERE
+        geodata._cities.title = 'Мурманск'
+    ;
+
