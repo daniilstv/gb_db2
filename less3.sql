742ce356536f709b49e82db530a55ba0dbb23e8f
daniilstv <daniilstv@gmail.com>
2019-02-18 23:47:51 +0300

    add 3.2 - Moscow region

--- less3.sql
+++ less3.sql
@@ -4,13 +4,19 @@ SELECT
     geodata._cities.title AS 'Город',
     geodata._regions.title AS 'Регион',
     geodata._countries.title AS 'Страна'
-
     FROM geodata._cities 
         LEFT JOIN (geodata._regions, geodata._countries)
             ON (geodata._cities.region_id = geodata._regions.id 
                 AND geodata._cities.country_id = geodata._countries.id)
-    
     WHERE
         geodata._cities.title = 'Мурманск'
     ;
 
+
+SELECT
+    geodata._cities.title AS 'Город'
+    FROM geodata._cities 
+        JOIN geodata._regions ON geodata._cities.region_id = geodata._regions.id
+    WHERE
+        geodata._regions.title = 'Московская область'
+    ;
